17928c4b53a5ed347fc21c13908cc4547579238b
yuralubinec <yuralubinec@gmail.com>
2017-01-24 11:45:24 +0200

    first commit

---
+++ src/main/java/com/uragan/configuration/AppInitializer.java
@@ -0,0 +1,39 @@
+package com.uragan.configuration;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.uragan.configuration.AppConfiguration;
+import com.uragan.filter.HeaderFilter;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+
+    return new Class [] {AppConfiguration.class};
+  }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+
+    return null;
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+
+    return new String [] {"/"};
+  }
+  
+  @Override
+  protected Filter[] getServletFilters() {
+    
+    CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+    characterEncodingFilter.setEncoding("UTF-8");
+    characterEncodingFilter.setForceEncoding(true);
+    return new Filter [] {characterEncodingFilter, new HeaderFilter()};
+  }
+}
