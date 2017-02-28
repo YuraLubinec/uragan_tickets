2573263e67b2e7876802b4183e787465da69fefa
yuralubinec <yuralubinec@gmail.com>
2017-02-28 17:17:43 +0200

    move sectors html representation to separate directives

--- src/main/webapp/resources/main-page/main-page.module.js
+++ src/main/webapp/resources/main-page/main-page.module.js
@@ -1,1 +1,13 @@
-angular.module('mainPage',[]);
+var mainPage = angular.module('mainPage', []);
+
+mainPage.directive('sectorAsc', function() {  
+  return {
+    templateUrl : 'resources/static/sectorASC.html'
+  };
+});
+
+mainPage.directive('sectorDesc', function() {  
+  return {
+    templateUrl : 'resources/static/sectorDESC.html'
+  };
+});
